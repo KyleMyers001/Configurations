b82115ac568ae3b5c37cbc9fb4a8101ec13cae21
KyleMyers001 <41594360+KyleMyers001@users.noreply.github.com>
2018-10-24 09:25:13 -0400

    Adding new gulp file.

---
+++ Gulp/Advanced-Features/gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const headerfooter = require('gulp-headerfooter');
+const imagemin = require('gulp-imagemin');
+const livereload = require('gulp-livereload');
+const log = require('fancy-log');
+const open = require('gulp-open');
+const run = require('gulp-run');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+
+gulp.task('sass', () => {
+  return gulp.src('src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(livereload());
+});
+
+gulp.task('html', () => {
+  gulp.src('src/*.html')
+    .pipe(headerfooter.header('src/partials/header.html'))
+    .pipe(headerfooter.footer('src/partials/footer.html'))
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+
+  gulp.src('src/partials/*.html')
+    .pipe(gulp.dest('dist/partials'))
+    .pipe(livereload());
+});
+
+gulp.task('scripts', () => {
+  gulp.src('src/scripts/*.js')
+    .pipe(uglify().on('error', function() { log.error('test'); }))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(livereload());
+});
+
+gulp.task('images', () => {
+  gulp.src('src/images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('server', () => {
+  return run('node index.js').exec();
+});
+
+gulp.task('watch', ['scripts', 'sass', 'html', 'images'], () => {
+  gulp.watch('src/scripts/*.js', ['scripts']);
+  gulp.watch('src/styles/*.scss', ['sass']);
+  gulp.watch('src/**/*.html', ['html']);
+  gulp.watch('src/images/*', ['images']);
+});
+
+gulp.task('uri', function(){
+  gulp.src(__filename)
+    .pipe(open({uri: 'http://localhost:3000'}));
+});
+
+gulp.task('serve', ['uri', 'watch'], () => {
+  livereload.listen();
+  gulp.start('server');
+});
+
+gulp.task('default', ['serve']);
