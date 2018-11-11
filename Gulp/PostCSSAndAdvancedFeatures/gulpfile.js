b11652f05be1929e243288aa0d6b4f8e23439334
KyleMyers001 <41594360+KyleMyers001@users.noreply.github.com>
2018-11-11 17:02:41 -0500

    Includes PostCSS configuration.

---
+++ Gulp/PostCSSAndAdvancedFeatures/gulpfile.js
@@ -0,0 +1,88 @@
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const headerfooter = require('gulp-headerfooter');
+const imagemin = require('gulp-imagemin');
+const postcss = require('gulp-postcss');
+const postcssPresetEnv = require('postcss-preset-env');
+const uglify = require('gulp-uglify');
+const util = require('gulp-util');
+const postcssplugins = [
+  postcssPresetEnv({
+    importFrom: './src/styles/partials/variables.css'
+  })
+];
+
+const cssminplugins = [
+  require('cssnano')
+];
+const isDevBuild = util.env.production !== true;
+
+gulp.task('html', () => {
+  return gulp.src('src/**/*.html')
+    .pipe(headerfooter.header('src/partials/header.html'))
+    .pipe(headerfooter.footer('src/partials/footer.html'))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', () => {
+  let build = gulp.src('src/images/**/*');
+  if(!isDevBuild) {
+    build = build.pipe(imagemin());
+  }
+  
+  return build.pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('css', () => {
+  let build = gulp.src('src/styles/*.css')
+    .pipe(postcss(postcssplugins))
+
+  if (!isDevBuild) {
+    build = build.pipe(postcss(cssminplugins))
+  }
+
+  return build.pipe(gulp.dest('dist/styles'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', () => {
+  let build = gulp.src('src/scripts/**/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }));
+
+  if (!isDevBuild) {
+    build = build.pipe(uglify().on('error', (uglify) => {
+      console.error(uglify.message);
+      gulp.emit('end');
+    }));
+  }
+  return build.pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('assets', () => {
+  return gulp.src('src/assets/**/*')
+    .pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('serve', [ 'watch'], () => {
+  return browserSync.init({
+    notify: false,
+    server: './dist'
+  });
+});
+
+gulp.task('build', ['scripts', 'css', 'html', 'images', 'assets']);
+
+gulp.task('watch', ['build'], () => {
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  gulp.watch('src/styles/**/*.css', ['css']);
+  gulp.watch('src/**/*.html', ['html']);
+  gulp.watch('src/images/**/*', ['images']);
+});
+
+gulp.task('default', ['serve']);
